a7562db740b8539f63ef0dd351118b18605f923a
2a5f <u2a5f@outlook.com>
2020-12-07 23:48:36 +0800

    lamp block

--- src/main/kotlin/co/volight/glacier/ficus/Ficus.kt
+++ src/main/kotlin/co/volight/glacier/ficus/Ficus.kt
@@ -23,12 +23,42 @@ fun init() {
 
 fun initBlocks() {
     LampBlock.White.reg()
+    LampBlock.Orange.reg()
+    LampBlock.Magenta.reg()
+    LampBlock.LightBlue.reg()
+    LampBlock.Yellow.reg()
+    LampBlock.Lime.reg()
+    LampBlock.Pink.reg()
+    LampBlock.Gray.reg()
+    LampBlock.LightGray.reg()
+    LampBlock.Cyan.reg()
+    LampBlock.Purple.reg()
+    LampBlock.Blue.reg()
+    LampBlock.Brown.reg()
+    LampBlock.Green.reg()
+    LampBlock.Red.reg()
+    LampBlock.Black.reg()
 
     Toilet.Oak.reg()
 }
 
 fun initItems() {
     LampBlock.White.regBlockItem()
+    LampBlock.Orange.regBlockItem()
+    LampBlock.Magenta.regBlockItem()
+    LampBlock.LightBlue.regBlockItem()
+    LampBlock.Yellow.regBlockItem()
+    LampBlock.Lime.regBlockItem()
+    LampBlock.Pink.regBlockItem()
+    LampBlock.Gray.regBlockItem()
+    LampBlock.LightGray.regBlockItem()
+    LampBlock.Cyan.regBlockItem()
+    LampBlock.Purple.regBlockItem()
+    LampBlock.Blue.regBlockItem()
+    LampBlock.Brown.regBlockItem()
+    LampBlock.Green.regBlockItem()
+    LampBlock.Red.regBlockItem()
+    LampBlock.Black.regBlockItem()
 
     Toilet.Oak.regBlockItem()
 }
--- src/main/kotlin/co/volight/glacier/ficus/blocks/light/LampBlock.kt
+++ src/main/kotlin/co/volight/glacier/ficus/blocks/light/LampBlock.kt
@@ -13,4 +13,109 @@ class LampBlock(settings: Settings): Block(settings), JoinItemGroup {
                     .sounds(BlockSoundGroup.GLASS).luminance { 15 })
         }
     }
+    object Black: BlockRegWithItem<LampBlock> {
+        override val name: String = "black_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.BLACK).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object LightGray: BlockRegWithItem<LampBlock> {
+        override val name: String = "light_gray_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.LIGHT_GRAY).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Gray: BlockRegWithItem<LampBlock> {
+        override val name: String = "gray_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.GRAY).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Blue: BlockRegWithItem<LampBlock> {
+        override val name: String = "blue_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.BLUE).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object LightBlue: BlockRegWithItem<LampBlock> {
+        override val name: String = "light_blue_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.LIGHT_BLUE).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Green: BlockRegWithItem<LampBlock> {
+        override val name: String = "green_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.GREEN).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Lime: BlockRegWithItem<LampBlock> {
+        override val name: String = "lime_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.LIME).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Cyan: BlockRegWithItem<LampBlock> {
+        override val name: String = "cyan_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.CYAN).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Magenta: BlockRegWithItem<LampBlock> {
+        override val name: String = "magenta_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.MAGENTA).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Orange: BlockRegWithItem<LampBlock> {
+        override val name: String = "orange_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.ORANGE).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Pink: BlockRegWithItem<LampBlock> {
+        override val name: String = "pink_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.PINK).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Red: BlockRegWithItem<LampBlock> {
+        override val name: String = "red_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.RED).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Purple: BlockRegWithItem<LampBlock> {
+        override val name: String = "purple_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.PURPLE).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Yellow: BlockRegWithItem<LampBlock> {
+        override val name: String = "yellow_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.YELLOW).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
+    object Brown: BlockRegWithItem<LampBlock> {
+        override val name: String = "brown_lamp_block"
+        override val impl: LampBlock by lazy {
+            LampBlock(Settings.of(Material.GLASS, MaterialColor.BROWN).strength(0.3f)
+                .sounds(BlockSoundGroup.GLASS).luminance { 15 })
+        }
+    }
 }
