37bed84e353e40a8cdce46c5cfe4a7c01c005d60
2a5f <u2a5f@outlook.com>
2020-12-08 14:48:56 +0800

    toilet water color

--- src/main/kotlin/co/volight/glacier/ficus/FicusClient.kt
+++ src/main/kotlin/co/volight/glacier/ficus/FicusClient.kt
@@ -4,16 +4,30 @@ import co.volight.glacier.ficus.Ficus.logName
 import co.volight.glacier.ficus.blocks.light.GlowGlass
 import co.volight.glacier.ficus.blocks.toilet.Toilet
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.fabricmc.fabric.api.client.render.ColorProviderRegistry
+import net.minecraft.block.BlockState
+import net.minecraft.client.color.world.BiomeColors
 import net.minecraft.client.render.RenderLayer
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.BlockRenderView
 
 @Suppress("unused")
 fun initClient() {
     initBlockLayer()
     Ficus.logger.info("$logName Block Layer initialized")
+
+    initBlockColors()
+    Ficus.logger.info("$logName Block Colors initialized")
 }
 
 fun initBlockLayer() {
     BlockRenderLayerMap.INSTANCE.putBlock(GlowGlass.impl, RenderLayer.getCutout())
 
     BlockRenderLayerMap.INSTANCE.putBlock(Toilet.Oak.impl, RenderLayer.getTranslucent())
+}
+
+fun initBlockColors() {
+    ColorProviderRegistry.BLOCK.register({ _: BlockState, world: BlockRenderView?, pos: BlockPos?, _: Int ->
+        if (world != null && pos != null) BiomeColors.getWaterColor(world, pos) else -1
+    }, Toilet.Oak.impl)
 }
